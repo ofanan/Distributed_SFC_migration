6514d8af9123e5577b3670b8019eb073026f5796
Itamar Cohen <ofanan@gmail.com>
2022-03-17 21:12:51 +0100

    dfdfdf

--- src/SimController.cc
+++ src/SimController.cc
@@ -4,6 +4,23 @@
 
 using namespace omnetpp;
 
+class Chain
+{
+public:
+    int id;
+    int curDatacenter; // Id of the datacenter currently hosting me
+    int nxtDatacenter; // Id of the datacenter scheduled to host me
+    int curLvl;        // Level of the datacenter currently hosting me
+    bool isRtChain;    // When true, this is a RT chain
+    bool isNew;        // When true, this chain is new (not currently scheduled to any datacenter)
+    Chain (int id);
+};
+
+Chain::Chain (int id)
+{
+    id = id;
+}
+
 class SimController : public cSimpleModule
 {
 private:
@@ -15,6 +32,8 @@ Define_Module(SimController);
 
 void SimController::initialize ()
 {
+    Chain *chain = new Chain (7);
+    EV << "The id of my chain is " << chain->id << "\n";
 }
 
 void SimController::handleMessage (cMessage *msg)
