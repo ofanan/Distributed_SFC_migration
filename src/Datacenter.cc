df7b530a4173c729eaec891ca81e0ac4727ec643
Itamar Cohen <ofanan@gmail.com>
2022-03-25 21:37:57 +0100

    dfgdf

--- src/Datacenter.cc
+++ src/Datacenter.cc
@@ -8,21 +8,21 @@ using namespace omnetpp;
 
 class Datacenter : public cSimpleModule
 {
-//public:
-//    int8_t numChildren = int(par("numChildren"));
-//    int8_t numParents;   //= int(par("numParents"));
-//    bool isRoot;//          = (numParents==0);
-//    bool isLeaf;//         = (numChildren==0);
-//    int16_t  availCpu;
-//    std::vector <int32_t> assignedchains;
-//    std::vector <int32_t> placedchains;
+public:
+    int8_t numChildren;
+    int8_t numParents;
+    bool isRoot;
+    bool isLeaf;
+    int16_t  availCpu;
+    std::vector <int32_t> assignedchains;
+//    std::vector <int32_t> placedchains; // For some reason, uncommenting this line makes a build-netw. error.
+    std::vector <cMessage*> endTransmissionEvent;
 
-//    ~Datacenter();
+    ~Datacenter();
 
 private:
 //    std::vector <cQueue>    outputQ;
 //    std::vector <bool>      outputQisBusy;
-//    std::vector <cMessage*> endTransmissionEvent;
         virtual void initialize();
         virtual void handleMessage (cMessage *msg);
     void startXmt (cPacket *pkt, int outputQnum);
@@ -62,18 +62,18 @@ Define_Module(Datacenter);
 //    simtime_t endTransmissionTime = gate("toChild[0]$o")->getTransmissionChannel()->getTransmissionFinishTime();
 //    scheduleAt(endTransmissionTime, endTransmissionEvent[outputQnum]);
 //}
-//
-//Datacenter::~Datacenter()
-//{
-//    for (int i(0); i < numParents + numChildren; i++) {
-//        cancelAndDelete(endTransmissionEvent[i]);
-//    }
-//}
-//
+
+Datacenter::~Datacenter()
+{
+    for (int i(0); i < numParents + numChildren; i++) {
+        cancelAndDelete(endTransmissionEvent[i]);
+    }
+}
+
 void Datacenter::initialize()
 {
 //    availCpu        = nonAugmentedCpuAtLvl[int(par("lvl"))]; // Consider rsrc aug here?
-//    assignedchains = {};
+    assignedchains = {};
 //    placedchains   = {};
 //    int8_t numChildren = int(par("numChildren"));
 //    int8_t numParents;   //= int(par("numParents"));
@@ -81,9 +81,9 @@ void Datacenter::initialize()
 //    bool isLeaf;//         = (numChildren==0);
 //    outputQ.             resize (numParents + numChildren);
 //    outputQisBusy.       resize (numParents + numChildren);
-//    endTransmissionEvent.resize (numParents + numChildren);
+    endTransmissionEvent.resize (numParents + numChildren);
 //    std::fill(outputQisBusy.begin(), outputQisBusy.end(), false);
-//    std::fill(endTransmissionEvent.begin(), endTransmissionEvent.end(), nullptr);
+    std::fill(endTransmissionEvent.begin(), endTransmissionEvent.end(), nullptr);
 //
 }
 //
