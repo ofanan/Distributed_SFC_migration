0fd18adad31e9414f368ac57df596869c05a49ac
Itamar Cohen <ofanan@gmail.com>
2022-03-26 09:05:06 +0100

    dfdf

--- src/Datacenter.cc
+++ src/Datacenter.cc
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <omnetpp.h>
 #include <vector>
+#include <set>
 #include "Parameters.h"
 
 using namespace omnetpp;
@@ -9,27 +10,58 @@ using namespace omnetpp;
 class Datacenter : public cSimpleModule
 {
 public:
-    int8_t numChildren;
-    int8_t numParents;
+    int16_t numChildren;
+    int16_t numParents;
     bool isRoot;
     bool isLeaf;
     int16_t  availCpu;
-    std::vector <int32_t> assignedchains;
-//    std::vector <int32_t> placedchains; // For some reason, uncommenting this line makes a build-netw. error.
+    std::set <int32_t> assignedchains;
+    std::set <int32_t> placedchains; // For some reason, uncommenting this line makes a build-netw. error.
     std::vector <cMessage*> endTransmissionEvent;
 
-    ~Datacenter();
+//    ~Datacenter();
 
 private:
 //    std::vector <cQueue>    outputQ;
 //    std::vector <bool>      outputQisBusy;
         virtual void initialize();
-        virtual void handleMessage (cMessage *msg);
-    void startXmt (cPacket *pkt, int outputQnum);
-    std::string qNumToOutputName (int8_t qNum);
+//        virtual void handleMessage (cMessage *msg);
+//    void startXmt (cPacket *pkt, int outputQnum);
+//    std::string qNumToOutputName (int8_t qNum);
 };
 
 Define_Module(Datacenter);
+
+
+void Datacenter::initialize()
+{
+    availCpu        = nonAugmentedCpuAtLvl[int(par("lvl"))]; // Consider rsrc aug here?
+      numChildren   = (int16_t) (par("numChildren"));
+      numParents    = (int16_t) (par("numParents"));
+//    bool isRoot;//          = (numParents==0);
+//    bool isLeaf;//         = (numChildren==0);
+//    outputQ.             resize (numParents + numChildren);
+//    outputQisBusy.       resize (numParents + numChildren);
+//    endTransmissionEvent.resize (numParents + numChildren);
+//    std::fill(outputQisBusy.begin(), outputQisBusy.end(), false);
+//    std::fill(endTransmissionEvent.begin(), endTransmissionEvent.end(), nullptr);
+//
+}
+
+//Datacenter::~Datacenter()
+//{
+//    for (int i(0); i < numParents + numChildren; i++) {
+//        cancelAndDelete(endTransmissionEvent[i]);
+//    }
+//}
+
+//
+//}
+//
+//void Datacenter::handleMessage (cMessage *msg)
+//{
+//    EV << "DC " << int (par ("id")) << " rcvd msg";
+//}
 
 //std::string Datacenter::qNumToOutputName (int8_t qNum)
 //{
@@ -63,36 +95,6 @@ Define_Module(Datacenter);
 //    scheduleAt(endTransmissionTime, endTransmissionEvent[outputQnum]);
 //}
 
-Datacenter::~Datacenter()
-{
-    for (int i(0); i < numParents + numChildren; i++) {
-        cancelAndDelete(endTransmissionEvent[i]);
-    }
-}
-
-void Datacenter::initialize()
-{
-//    availCpu        = nonAugmentedCpuAtLvl[int(par("lvl"))]; // Consider rsrc aug here?
-    assignedchains = {};
-//    placedchains   = {};
-//    int8_t numChildren = int(par("numChildren"));
-//    int8_t numParents;   //= int(par("numParents"));
-//    bool isRoot;//          = (numParents==0);
-//    bool isLeaf;//         = (numChildren==0);
-//    outputQ.             resize (numParents + numChildren);
-//    outputQisBusy.       resize (numParents + numChildren);
-    endTransmissionEvent.resize (numParents + numChildren);
-//    std::fill(outputQisBusy.begin(), outputQisBusy.end(), false);
-    std::fill(endTransmissionEvent.begin(), endTransmissionEvent.end(), nullptr);
-//
-}
-//
-//}
-//
-void Datacenter::handleMessage (cMessage *msg)
-{
-    EV << "DC " << int (par ("id")) << " rcvd msg";
-}
 //
 //
 //
