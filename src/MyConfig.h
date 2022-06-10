d2d1358e51817ee9e392e47c31db16653eb2630c
ofanan <ofanan@gmail.com>
2022-06-10 19:04:04 +0200

    Compiled

--- src/MyConfig.h
+++ src/MyConfig.h
@@ -27,6 +27,7 @@ using namespace std;
 class Chain;
 class ChainHash;
 
+const int TOY=0, CITY=1;
 const string netType = "city";
 const vector <Cost_t> RT_ChainCostAtLvl[2] = {{4, 3, 2, 1}, {7,7,7,7}};
 
