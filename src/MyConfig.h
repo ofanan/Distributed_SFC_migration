20383323a21ee7ece40513fad28c800ca5eeded1
ofanan <ofanan@gmail.com>
2022-06-14 12:36:59 +0200

    Running, w ~3 times srsc or=ug. Hi,

--- src/MyConfig.h
+++ src/MyConfig.h
@@ -26,6 +26,7 @@ using namespace std;
 
 class Chain;
 class ChainHash;
+
 const string traceFileName = "results/poa_files/Monaco_0829_0830_20secs_Telecom.poa";
 
 const int  NO_LOG=0, BASIC_LOG=1, DETAILED_LOG=2, VERY_DETAILED_LOG = 3; // levels of log to be written to the log file.
@@ -36,7 +37,7 @@ const vector <Cost_t> RT_ChainCostAtLvl     [] = {{544, 278, 164}, 							{100, 
 const vector <Cost_t> Non_RT_ChainCostAtLvl [] = {{544, 278, 148, 86, 58, 47}, 	{100, 10, 1}, {68, 40, 29}, 		 {68, 40, 29}};
 const vector <Cpu_t>  RT_ChainMu_u 				  [] = {{17, 17, 19}, 									{1, 	1 	 }, {17, 17, 19}, 		 {17, 17, 19}};
 const vector <Cpu_t>  Non_RT_ChainMu_u 		  [] = {{17, 17, 17, 17, 17, 17},			{1, 	1, 	1}, {17, 17, 17}, 		 {17, 17, 17}};
-const vector <Cpu_t>  nonAugmentedCpuAtLeaf    = {842, 94, 1,	30,}; 
+const vector <Cpu_t>  nonAugmentedCpuAtLeaf    = {1842, 94, 1,	30,}; //{842, 94, 1,	30,}; 
 const int uniformChainMisgCost = 600;
 
 // Parameters determining how to define whether a new generated chain is RT or not
