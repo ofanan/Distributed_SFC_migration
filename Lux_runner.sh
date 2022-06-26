ae2c50fc305cf96f6abc5af5079ef66edcff4575
ofanan <ofanan@gmail.com>
2022-06-26 13:26:38 +0200

    Added cleaner

--- Lux_runner.sh
+++ Lux_runner.sh
@@ -1,9 +1,9 @@
 LANG=C
-opp_makemake -f --deep
-mkdir -p out/clang-release
-chmod 777 out out/clang-release
-chmod 777 Makefile
-make -i
-clear
+#opp_makemake -f --deep
+#mkdir -p out/clang-release
+#chmod 777 out out/clang-release
+#chmod 777 Makefile
+#make -i
+#clear
 out/clang-release/Distributed_SFC_migration -f Common.ini Lux.ini -u Cmdenv
 
---
+++ cleaner.sh
@@ -0,0 +1,5 @@
+\rm slurm*
+\rm Lux_0829_0830_60secs_post.poa.*
+\rm Monaco_0829_0830_20secs_Telecom.poa.*
+\rm UniformTree.poa.*
+
