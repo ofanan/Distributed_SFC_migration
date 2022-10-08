69f2164953181f71d550555b3f02c14736ee76da
ofanan <ofanan@gmail.com>
2022-10-08 17:20:00 +0200

    Stable version, with the correct (re)sched coding. Still high cpu.

--- src/Datacenter.cc
+++ src/Datacenter.cc
@@ -906,11 +906,13 @@ void Datacenter::handleBottomUpPktAsync ()
 	}	
 	isInBuAccumDelay = true;
 	// schedule a BU run
-	if (bottomUpEvent!=nullptr) {// && reshAsyncEvent==nullptr) { // if there's already a scheduled BU event/reshAsync event, no need to schedule a new one
-		cancelAndDelete (bottomUpEvent);
+//	if (bottomUpEvent!=nullptr) {// && reshAsyncEvent==nullptr) { // if there's already a scheduled BU event/reshAsync event, no need to schedule a new one
+//		cancelAndDelete (bottomUpEvent);
+//	}
+	if (bottomUpEvent==nullptr) {// && reshAsyncEvent==nullptr) { // if there's already a scheduled BU event/reshAsync event, no need to schedule a new one
+		bottomUpEvent = new cMessage ("bottomUp");
+		scheduleAt (simTime() + MyConfig::BU_ACCUM_DELAY_OF_LVL[lvl], bottomUpEvent); 
 	}
-	bottomUpEvent = new cMessage ("bottomUp");
-	scheduleAt (simTime() + MyConfig::BU_ACCUM_DELAY_OF_LVL[lvl], bottomUpEvent); 
 }
 
 /*************************************************************************************************************************************************
